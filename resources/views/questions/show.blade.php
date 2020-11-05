df3a990d02c000e2d515d6e356d7fde2f69fd938
unknown <prtk.varma@gmail.com>
2020-11-04 18:20:13 -0600

    created answer form

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,5 +5,21 @@
 <div class="container">
     <h1>{{ $question->title }}</h1>
     <p class="lead">{{ $question->description }}</p>
+
+
+    <hr>
+
+    <div class="col-md-8">
+    <form action="{{ route('answers.store') }}" method="post">
+        @csrf
+        <div class="form-group">
+            <label for="content">Description: </label>
+            <textarea class="form-control" name="content" id="content" rows="10" ></textarea>
+            <input type="hidden" value="{{ $question->id }}" name="question_id">
+            <br>
+            <button  type="submit" class="btn btn-primary btn-md">Submit Answer</button>
+        </div>
+    </form>
+    </div>
 </div>
 @endsection
